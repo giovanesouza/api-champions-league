d238496b404279611323f01b045ba0d379dc7655
Giovane Souza <developergiovanesouza@gmail.com>
2024-07-02 20:52:39 -0300

    refact: ajustes na estrutura

--- src/models/player-model.ts
+++ src/models/player-model.ts
@@ -1,4 +1,17 @@
 export interface PlayerModel {
     id: number;
     name: string;
-};
+    club: string;
+    nationality: string;
+    position: string;
+    statistics: {
+      Overall: number;
+      Pace: number;
+      Shooting: number;
+      Passing: number;
+      Dribbling: number;
+      Defending: number;
+      Physical: number;
+    };
+  }
+  
