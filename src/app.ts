24d66d32a3c25943331f0307fb6e463b6b61fd4f
Giovane Souza <developergiovanesouza@gmail.com>
2024-07-03 00:38:04 -0300

    build: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./routes/routes";
+import cors from "cors";
 
 // Cria e exporta a função por Default
 export default function createApp() {
   const app = express(); // cria aplicação express
   app.use(express.json()); // Utiliza middleware que converte o corpo das requisições para Content-Type JSON
+  app.use(cors()); // controla quem pode acessar a API
   app.use("/api", router); // importa todas as rotas
 
   return app;
